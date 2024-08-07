1bfe9342a39d0efcab85c4603547fb9efe964c86
José Ojeda <minecraftersov@gmail.com>
2024-08-07 19:20:22 -0400

    wow now the display kind of works with the sensor

--- sketch.ino
+++ sketch.ino
@@ -25,10 +25,13 @@ void loop() {
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
-
   const int duration = pulseIn(echo, HIGH);
+
   Serial1.print("Distance in CM: ");
   Serial1.println(duration / 58);
+
+  tm.display(0, (duration / 58));
+/*
   tm.display(0, (counter / 1000) % 10);
   tm.display(1, (counter / 100) % 10);
   tm.display(2, (counter / 10) % 10);
@@ -38,5 +41,6 @@ void loop() {
   if (counter == 10000) {
     counter = 0;
   }
+  */
   delay(1);
 }
