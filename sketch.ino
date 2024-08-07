811a40dc25fac52926f42441f400346576ac9dfe
José Ojeda <minecraftersov@gmail.com>
2024-08-07 19:16:01 -0400

    wow the sensor works awesome i love it

--- sketch.ino
+++ sketch.ino
@@ -12,6 +12,8 @@ TM1637 tm(CLK, DIO);
 void setup() {
   tm.init();
   tm.set(BRIGHT_TYPICAL);
+  Serial1.begin(115200);
+  Serial1.println("holi");
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
 }
@@ -24,12 +26,9 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
 
-  int duration = pulseIn(echo, HIGH);
-  Serial.print("Distance in CM: ");
-  Serial.println(duration / 58);
-  Serial.print("Distance in inches: ");
-  Serial.println(duration / 148);
-  /*
+  const int duration = pulseIn(echo, HIGH);
+  Serial1.print("Distance in CM: ");
+  Serial1.println(duration / 58);
   tm.display(0, (counter / 1000) % 10);
   tm.display(1, (counter / 100) % 10);
   tm.display(2, (counter / 10) % 10);
@@ -39,6 +38,5 @@ void loop() {
   if (counter == 10000) {
     counter = 0;
   }
-*/
-  delay(100);
+  delay(1);
 }
